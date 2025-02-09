2d79d4ae1e2592aeccabf7ae260eaf7234e24458
Tirth Patel <tirthpatelcse@gmail.com>
2025-02-10 02:11:17 +0530

    create table for tender listing

--- src/app/tenders/page.tsx
+++ src/app/tenders/page.tsx
@@ -1,84 +1,40 @@
+import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 
-const tenders = [
-  {
-    dtVoucherDate: "2025-02-09 08:10:42.823",
-    stTenderName: "Tender Name",
-    stTenderType: "Single Stone",
-    dcNotePercentage: 106,
+export default async function Tenders() {
+  const tenders = await prisma.tender.findMany({
+    select: {
+      id: true,
+      dtVoucherDate: true,
+      stTenderName: true,
+      stTenderType: true,
+      dcNotePercentage: true,
+      stLotNo: true,
+      stRoughName: true,
+      inTotalRoughPcs: true,
+      dcTotalRoughCts: true,
+      dcRoughSize: true,
+      dcRoughPrice: true,
+      dcRoughTotal: true,
+      dcBidPrice: true,
+      dcResultCost: true,
+      dcFinalCostPrice: true,
+      dcTotalAmount: true,
+      dcResultPerCt: true,
+      dcFinalBidPrice: true,
+      dcResultTotal: true,
+      dcFinalTotalAmount: true,
+    },
+    orderBy: {
+      dtVoucherDate: "desc",
+    },
+  });
 
-    stLotNo: "FS39",
-    stRoughName: "Rough Name",
-    inTotalRoughPcs: 10,
-    dcTotalRoughCts: 37.78,
-    dcRoughPrice: 870.89,
-    dcRoughTotal: 872.89,
-
-    dcBidPrice: 8169.51,
-    dcTotalAmount: 40520.75,
-
-    dcResultCost: 18000.0,
-    dcResultPerCt: 10438.53,
-    dcResultTotal: 51775.11,
-
-    dcFinalCostPrice: 17999.0,
-    dcFinalBidPrice: 8169.0,
-    dcFinalTotalAmount: 40518.24,
-  },
-  {
-    dtVoucherDate: "2025-02-10 08:10:42.823",
-    stTenderName: "Tender 2",
-    stTenderType: "Type 1",
-    dcNotePercentage: 1.2,
-
-    stRoughName: "Rough 1",
-    inTotalRoughPcs: 1,
-    dcTotalRoughCts: 1.2,
-    dcRoughPrice: 1.2,
-    dcRoughTotal: 1.2,
-
-    dcBidPrice: 1.2,
-    dcTotalAmount: 1.2,
-
-    dcResultCost: 1.2,
-    dcResultPerCt: 1.2,
-    dcResultTotal: 1.2,
-
-    dcFinalCostPrice: 1.2,
-    dcFinalBidPrice: 1.2,
-    dcFinalTotalAmount: 1.2,
-  },
-  {
-    dtVoucherDate: "2021-09-03",
-    stTenderName: "Tender 3",
-    stTenderType: "Type 1",
-    dcNotePercentage: 1.2,
-
-    stRoughName: "Rough 1",
-    inTotalRoughPcs: 1,
-    dcTotalRoughCts: 1.2,
-    dcRoughPrice: 1.2,
-    dcRoughTotal: 1.2,
-
-    dcBidPrice: 1.2,
-    dcTotalAmount: 1.2,
-
-    dcResultCost: 1.2,
-    dcResultPerCt: 1.2,
-    dcResultTotal: 1.2,
-
-    dcFinalCostPrice: 1.2,
-    dcFinalBidPrice: 1.2,
-    dcFinalTotalAmount: 1.2,
-  },
-];
-
-export default function Tenders() {
   return (
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
-          <h1 className="text-base font-semibold leading-6 text-gray-900">
+          <h1 className="text-base font-semibold leading-6 text-gray-700">
             Tenders
           </h1>
           <p className="mt-2 text-sm text-gray-700">Manage your tenders.</p>
@@ -100,32 +56,32 @@ export default function Tenders() {
                 <tr>
                   <th
                     scope="col"
-                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-700 sm:pl-0 w-fit"
                   >
                     Details
                   </th>
                   <th
                     scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-700"
                   >
                     Rough Details
                   </th>
                   <th
                     scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-700"
                   >
                     Cost Details
                   </th>
                   <th
                     scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-700"
                   >
                     Result
                   </th>
 
                   <th
                     scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-700"
                   >
                     Final
                   </th>
@@ -136,110 +92,146 @@ export default function Tenders() {
               </thead>
               <tbody className="divide-y divide-gray-200 bg-white">
                 {tenders.map((tender) => (
-                  <tr key={tender.dtVoucherDate} className="bg-white">
-                    <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
+                  <tr key={tender.id} className="bg-white">
+                    <td className="max-w-40 truncate whitespace-nowrap py-3 px-3 text-sm sm:pl-0">
                       <div className="flex items-center">
                         <div className="grid grid-cols-2 gap-4 divide-x divide-gray-400">
                           <div>
                             <div className="flex gap-2">
-                              {/* <span className="font-medium text-gray-900">
+                              {/* <span className="font-medium text-gray-700">
                                 Voucher Date:
                               </span> */}
-                              <span className="text-gray-500">
+                              <span className="text-gray-700">
                                 {new Date(
                                   tender.dtVoucherDate
                                 ).toLocaleDateString()}
                               </span>
                             </div>
                             <div className="flex gap-2">
-                              {/* <span className="font-medium text-gray-900">
+                              {/* <span className="font-medium text-gray-700">
                                 Tender Name:
                               </span> */}
-                              <span className="text-gray-500">
+                              <span className="text-gray-700">
                                 {tender.stTenderName}
                               </span>
                             </div>
                           </div>
                           <div className="pl-4">
                             <div className="flex gap-2">
-                              {/* <span className="font-medium text-gray-900">
+                              {/* <span className="font-medium text-gray-700">
                                 Tender Type:
                               </span> */}
-                              <span className="text-gray-500">
+                              <span className="text-gray-700">
                                 {tender.stTenderType}
                               </span>
                             </div>
                             <div className="flex gap-2">
-                              {/* <span className="font-medium text-gray-900">
+                              {/* <span className="font-medium text-gray-700">
                                 Note %:
                               </span> */}
-                              <span className="text-gray-500">
-                                {tender.dcNotePercentage}
+                              <span className="text-gray-700">
+                                {tender.dcNotePercentage
+                                  ? String(tender.dcNotePercentage)
+                                  : ""}
                               </span>
                             </div>
                           </div>
                         </div>
                       </div>
                     </td>
-                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                      <div className="flex divide-x gap-4 divide-gray-400">
-                        <div className=" text-gray-500">{tender.stLotNo}</div>
-                        <div className="pl-4 text-gray-500">
-                          {tender.stRoughName}
-                        </div>
-                      </div>
-                      <div className="flex divide-x gap-4 divide-gray-400">
-                        <div className=" text-gray-500">
-                          {tender.inTotalRoughPcs}
-                        </div>
-                        <div className="pl-4 text-gray-500">
-                          {tender.dcTotalRoughCts}
-                        </div>
-                      </div>
-                      <div>
-                        <div className="mt-1 text-gray-500">
-                          {tender.dcRoughPrice}
-                        </div>
-
-                        <div className="mt-1 text-gray-500">
-                          {tender.dcRoughTotal}
+                    <td className="max-w-40 truncate whitespace-nowrap px-3 py-3 text-sm text-gray-700">
+                      <div className="flex items-center">
+                        <div className="grid grid-cols-2 gap-4 divide-x divide-gray-400">
+                          <div className="w-10">
+                            <div className="flex gap-2">
+                              <span className="text-gray-700">
+                                {tender.stLotNo}
+                              </span>
+                            </div>
+                            <div className="flex gap-2">
+                              <span className="text-gray-700">
+                                Pcs: {tender.inTotalRoughPcs}
+                              </span>
+                            </div>
+                          </div>
+                          <div className="pl-4">
+                            <div className="flex gap-2">
+                              {/* <span className="font-medium text-gray-700">
+                                Tender Type:
+                              </span> */}
+                              <span className="text-gray-700">
+                                {tender.stRoughName}
+                              </span>
+                            </div>
+                            <div className="flex gap-2">
+                              {/* <span className="font-medium text-gray-700">
+                                Note %:
+                              </span> */}
+                              <span className="text-gray-700">
+                                Cts: {tender.dcTotalRoughCts
+                                  ? String(tender.dcTotalRoughCts)
+                                  : ""}
+                              </span>
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </td>
-                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                      <div className="text-gray-900">{tender.dcBidPrice}</div>
-                      <div className="mt-1 text-gray-500">
-                        {tender.dcTotalAmount}
+                    <td className="max-w-40 truncate whitespace-nowrap px-3 py-3 text-sm text-gray-700">
+                      <div className="text-gray-700">
+                        Bid: {tender.dcBidPrice ? String(tender.dcBidPrice) : ""}
+                      </div>
+                      <div className="mt-1 text-gray-700">
+                        Total: {tender.dcTotalAmount
+                          ? String(tender.dcTotalAmount)
+                          : ""}
                       </div>
                     </td>
-                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                      <div className="text-gray-900">{tender.dcResultCost}</div>
-                      <div className="mt-1 text-gray-500">
-                        {tender.dcResultPerCt}
+                    <td className="max-w-40 truncate whitespace-nowrap px-3 py-3 text-sm text-gray-700">
+                      <div className="text-gray-700">
+                        Cost: {tender.dcResultCost ? String(tender.dcResultCost) : ""}
                       </div>
-                      <div className="mt-1 text-gray-500">
-                        {tender.dcResultTotal}
+                      <div className="mt-1 text-gray-700">
+                        Result / Ct: {tender.dcResultPerCt
+                          ? String(tender.dcResultPerCt)
+                          : ""}
                       </div>
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                      <div className="text-gray-900">
-                        {tender.dcFinalCostPrice}
-                      </div>
-                      <div className="mt-1 text-gray-500">
-                        {tender.dcFinalBidPrice}
-                      </div>
-                      <div className="mt-1 text-gray-500">
-                        {tender.dcFinalTotalAmount}
+                      <div className="mt-1 text-gray-700">
+                        Total: {tender.dcResultTotal
+                          ? String(tender.dcResultTotal)
+                          : ""}
                       </div>
                     </td>
-                    <td className="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                    <td className="max-w-20 truncate whitespace-nowrap px-3 py-3 text-sm text-gray-700">
+                      <div className="text-gray-700">
+                        Cost: {tender.dcFinalCostPrice
+                          ? String(tender.dcFinalCostPrice)
+                          : ""}
+                      </div>
+                      <div className="mt-1 text-gray-700">
+                        Bid: {tender.dcFinalBidPrice
+                          ? String(tender.dcFinalBidPrice)
+                          : ""}
+                      </div>
+                      <div className="mt-1 text-gray-700">
+                        Total: {tender.dcFinalTotalAmount
+                          ? String(tender.dcFinalTotalAmount)
+                          : ""}
+                      </div>
+                    </td>
+                    <td className="max-w-40 truncaterelative whitespace-nowrap py-3 px-3 text-right text-sm font-medium sm:pr-0">
                       <a
                         href="#"
                         className="text-indigo-600 hover:text-indigo-900"
                       >
                         Edit
                         <span className="sr-only">
-                          , {tender.dtVoucherDate}
+                          ,{" "}
+                          {tender.dtVoucherDate
+                            ? new Date(
+                                tender.dtVoucherDate
+                              ).toLocaleDateString()
+                            : ""}
                         </span>
                       </a>
                       <a
@@ -248,7 +240,12 @@ export default function Tenders() {
                       >
                         Delete
                         <span className="sr-only">
-                          , {tender.dtVoucherDate}
+                          ,{" "}
+                          {tender.dtVoucherDate
+                            ? new Date(
+                                tender.dtVoucherDate
+                              ).toLocaleDateString()
+                            : ""}
                         </span>
                       </a>
                     </td>
